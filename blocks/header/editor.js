8745d5bbbd736e1cf17630a628ba007ef724de83
davidmc.io <hi@davidmc.io>
2024-01-28 16:20:34 -0600

    Initial Commit...

--- blocks/header/editor.js
+++ blocks/header/editor.js
@@ -161,8 +161,8 @@ wp.domReady(function() {
         // Create your custom interface with a large post title field
         const customInterface = `
         <div class="editor-head">
-            <div id="featured-image-container" title="Change cover" data-image-id="0"></div>
-            <button id="featured-image-button" class="button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#32a85a" d="M13 19C13 19.7 13.13 20.37 13.35 21H5C3.9 21 3 20.11 3 19V5C3 3.9 3.9 3 5 3H19C20.11 3 21 3.9 21 5V13.35C20.37 13.13 19.7 13 19 13V5H5V19H13M13.96 12.29L11.21 15.83L9.25 13.47L6.5 17H13.35C13.75 15.88 14.47 14.91 15.4 14.21L13.96 12.29M20 18V15H18V18H15V20H18V23H20V20H23V18H20Z" /></svg>Add cover</button>
+            <div id="featured-image-container" title="Change cover" data-image-id="0" style="display:none;"></div>
+            <button id="featured-image-button" class="button" style="display:none;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#32a85a" d="M13 19C13 19.7 13.13 20.37 13.35 21H5C3.9 21 3 20.11 3 19V5C3 3.9 3.9 3 5 3H19C20.11 3 21 3.9 21 5V13.35C20.37 13.13 19.7 13 19 13V5H5V19H13M13.96 12.29L11.21 15.83L9.25 13.47L6.5 17H13.35C13.75 15.88 14.47 14.91 15.4 14.21L13.96 12.29M20 18V15H18V18H15V20H18V23H20V20H23V18H20Z" /></svg>Add cover</button>
             <div class="title-field">
                 <button id="icon-toggle-button"></button>
                 <input type="text" id="postTitle" name="editor-post-title" style="width: 100%; font-size: 24px; font-weight: bold; font-family: sans-serif;" value="${currentTitle}" placeholder="Entry Title..." />
